9ce12147c347fc0298144fc05745a2f6ab073e0c
buhlenngubane <buhlenngubane@gmail.com>
2024-03-20 18:38:32 +0200

    Fix reset password

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     path('signup/', views.UserCreateView.as_view(), name='signup'),
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name="logout"),
-    path('password_reset/', views.UserResetPasswordView.as_view(), name="password-reset"),
-    path('password-reset/done/', views.UserResetPasswordDoneView.as_view(),name='password_reset_done'),
-    path('password-reset/<uidb64>/<token>/', views.UserResetPasswordConfirmView.as_view(),name='password_reset_confirm'),
-    path('password-reset-complete/', views.UserResetPasswordCompleteView.as_view(),name='password_reset_complete'),
+    path('password_reset/', views.UserResetPasswordView.as_view(), name="password_reset"),
+    path('password_reset/done/', views.UserResetPasswordDoneView.as_view(),name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', views.UserResetPasswordConfirmView.as_view(),name='password_reset_confirm'),
+    path('password_reset_complete/', views.UserResetPasswordCompleteView.as_view(),name='password_reset_complete'),
     path('profile/<pk>', views.UserDetailView.as_view(), name='profile'),
 ]
